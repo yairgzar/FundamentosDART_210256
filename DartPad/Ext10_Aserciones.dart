b482910d615ccbf2d5367c08316752b0970e9b3e
Yair Salazar <210256@utxicotepec.edu.mx>
2024-10-08 10:34:41 -0600

    Practica 12
    Agregacion al ejercicio 10: Tipos de Datos Dinamicos

---
+++ DartPad/Ext10_Aserciones.dart
@@ -0,0 +1,33 @@
+void main(){
+  final mySquare = Square(side:10);
+
+  
+  print("""El lado del cuadradoes de:: ${mySquare._side}
+  au area es de: ${ mySquare.area}""");
+
+  mySquare.side = -5;
+  print("""El lado del cuadrado es de:: ${mySquare._side}
+  au area es de: ${ mySquare.area}""");
+}
+
+class Square {
+  double _side;
+  
+  Square({ required double side })
+    : assert(side > 0, 'El valor del lado deber ser mayor a 0'),
+  _side = side;
+
+  double get area {
+      return _side * _side;
+  }
+
+  set side(double value){
+    print("setting new value $value");
+      if (value < 0) throw 'Value must be >=0';
+    _side = value;
+  }
+  
+  double calculateArea() {
+  return _side * _side;
+  }
+}
